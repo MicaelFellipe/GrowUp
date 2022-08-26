cfd765980ba10ca6f457764fe18745a9addbc1fe
Micael Fellipe <micaelsteven79@gmail.com>
2022-08-26 15:28:43 -0300

    first commit

---
+++ utils/constants.tsx
@@ -0,0 +1,26 @@
+import { RiMentalHealthLine } from 'react-icons/ri';
+import { MdAttachMoney } from 'react-icons/md';
+import { IoMdBusiness, IoMdFitness } from 'react-icons/io';
+
+export const topics = [
+  {
+    name: 'finance',
+    icon: <MdAttachMoney />,
+  },
+  {
+    name: 'entrepreneurship',
+    icon: <IoMdBusiness />,
+  },
+  {
+    name: 'mental-health',
+    icon: <RiMentalHealthLine />,
+  },
+  {
+    name: 'workout',
+    icon: <IoMdFitness />,
+  },
+];
+
+export const footerList1 = ['About', 'Newsroom', 'Store', 'Contact', 'Carrers', 'ByteDance', 'Creator Directory']
+export const footerList2 = [ 'GrowUp for Good','Advertise','Developers','Transparency','GrowUp Rewards' ]
+export const footerList3 = [ 'Help', 'Safety', 'Terms', 'Privacy', 'Creator Portal', 'Community Guidelines' ]
